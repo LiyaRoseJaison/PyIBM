491b6af167a544b2028ec11b5bf002aeafaa5d5b
LiyaRoseJaison <61771217+LiyaRoseJaison@users.noreply.github.com>
2024-07-17 22:20:56 +0530

    Update DV0101EN-Final_Assign_Part_2.py

--- DV0101EN-Final_Assign_Part_2.py
+++ DV0101EN-Final_Assign_Part_2.py
@@ -180,7 +180,7 @@ def update_output_container(selected_statistics, input_year):
         return [
                 html.Div(className='chart-item', children=[html.Div(children=Y_chart1),html.Div(children=Y_chart2)],style={'display':'flex'}),
                 html.Div(className='chart-item', children=[html.Div(children=Y_chart3),html.Div(children=Y_chart4)],style={'display':'flex'})
-        
+	]
     else:
         return None
 
